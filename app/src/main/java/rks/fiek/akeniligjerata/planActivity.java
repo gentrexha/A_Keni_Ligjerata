2893db48a4d014394bf645b3f47a1ce52ed51f52
Gent Rexha <gentr@stud.ntnu.no>
2016-10-02 15:32:28 +0200

    added second activity

---
+++ app/src/main/java/rks/fiek/akeniligjerata/planActivity.java
@@ -0,0 +1,15 @@
+package rks.fiek.akeniligjerata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class planActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+    }
+}

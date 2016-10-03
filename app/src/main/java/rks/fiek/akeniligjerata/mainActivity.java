47d80909e69726f693b6a302e78282d44c38e8a9
Gent Rexha <gentr@stud.ntnu.no>
2016-10-03 12:47:24 +0200

    added a check on location service before starting google maps to prevent crash

--- app/src/main/java/rks/fiek/akeniligjerata/mainActivity.java
+++ app/src/main/java/rks/fiek/akeniligjerata/mainActivity.java
@@ -1,12 +1,19 @@
 package rks.fiek.akeniligjerata;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class mainActivity extends AppCompatActivity
 {
+    private int locationRequestCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -23,7 +30,16 @@ public class mainActivity extends AppCompatActivity
     public void btnMapOnClick(View v)
     {
         Intent intent = new Intent(this, mapsActivity.class);
-        startActivity(intent);
+        // Asks for permission to use location services
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},locationRequestCode);
+            // locationRequestCode is an app-defined int constant. The callback method gets the result of the request.
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            startActivity(intent);
+        else
+            Toast.makeText(getApplicationContext(), "Maps doesn't work without location access!", Toast.LENGTH_LONG).show();
     }
 
 }

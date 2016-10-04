bab34c28a89423d6a7a15f1b5bd19e879c622da5
Gent Rexha <gentr@stud.ntnu.no>
2016-10-04 13:48:48 +0200

    added a few png for availability of class rooms

--- app/src/main/java/rks/fiek/akeniligjerata/planActivity.java
+++ app/src/main/java/rks/fiek/akeniligjerata/planActivity.java
@@ -13,6 +13,7 @@ public class planActivity extends AppCompatActivity {
 
     TouchImageView imgv4thFloor;
     TouchImageView imgv4thFloor_Area;
+    TouchImageView imgvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +21,14 @@ public class planActivity extends AppCompatActivity {
         setContentView(R.layout.activity_plan);
         imgv4thFloor = (TouchImageView)findViewById(R.id.imgvPlan);
         imgv4thFloor_Area = (TouchImageView)findViewById(R.id.imgvPlan_Area);
+        imgvTest = (TouchImageView)findViewById(R.id.imgvTest);
 
+        // Enables that both pictures zoom at the same time
         imgv4thFloor.setOnTouchImageViewListener(new TouchImageView.OnTouchImageViewListener() {
             @Override
             public void onMove() {
                 imgv4thFloor_Area.setZoom(imgv4thFloor);
+                imgvTest.setZoom(imgv4thFloor);
             }
         });
 
@@ -35,6 +39,14 @@ public class planActivity extends AppCompatActivity {
             }
         });
 
+        imgvTest.setOnTouchImageViewListener(new TouchImageView.OnTouchImageViewListener() {
+            @Override
+            public void onMove() {
+                imgv4thFloor.setZoom(imgvTest);
+            }
+        });
+
+        //
         imgv4thFloor.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent)

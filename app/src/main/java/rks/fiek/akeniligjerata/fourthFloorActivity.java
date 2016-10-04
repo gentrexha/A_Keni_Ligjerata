e4c92afdd8f3f1c93cbfeaf9cc9d361cd1eaba5a
Gent Rexha <gentr@stud.ntnu.no>
2016-10-04 20:28:41 +0200

    fixed fourthflooractivity class

--- app/src/main/java/rks/fiek/akeniligjerata/fourthFloorActivity.java
+++ app/src/main/java/rks/fiek/akeniligjerata/fourthFloorActivity.java
@@ -18,20 +18,11 @@ public class fourthFloorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/rks/fiek/akeniligjerata/planActivity.java
-        setContentView(R.layout.activity_plan);
-=======
         setContentView(R.layout.activity_fourthfloor);
->>>>>>> origin/master:app/src/main/java/rks/fiek/akeniligjerata/fourthFloorActivity.java
-
+        
         imgv4thFloor = (ImageView)findViewById(R.id.imgvPlan);
         imgv4thFloor_Area = (ImageView)findViewById(R.id.imgvPlan_Area);
 
-<<<<<<< HEAD:app/src/main/java/rks/fiek/akeniligjerata/planActivity.java
-
-        //
-=======
->>>>>>> origin/master:app/src/main/java/rks/fiek/akeniligjerata/fourthFloorActivity.java
         imgv4thFloor.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent)

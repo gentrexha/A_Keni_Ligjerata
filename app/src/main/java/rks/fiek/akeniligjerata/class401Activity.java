7b1d67ea7524680e039f39d5609212d83b47e1d6
Gent Rexha <gentr@stud.ntnu.no>
2016-10-05 20:15:46 +0200

    added activities for each class room

---
+++ app/src/main/java/rks/fiek/akeniligjerata/class401Activity.java
@@ -0,0 +1,13 @@
+package rks.fiek.akeniligjerata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class class401Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class401);
+    }
+}

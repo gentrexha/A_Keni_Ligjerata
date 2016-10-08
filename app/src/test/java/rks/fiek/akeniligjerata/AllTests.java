a97df6785664e6e519c1e001a3fa9a7de0e51463
valentina_shabani <valentinashabani02@gmail.com>
2016-10-08 19:12:47 +0200

    Added a junit test

---
+++ app/src/test/java/rks/fiek/akeniligjerata/AllTests.java
@@ -0,0 +1,15 @@
+package rks.fiek.akeniligjerata;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(AllTests.class.getName());
+
+        suite.addTestSuite(mapActivityTest.class);
+
+        return suite;
+    }
+}

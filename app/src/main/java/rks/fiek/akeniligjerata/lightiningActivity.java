6976676a7858aeac562d3d641e87698b0ced60ce
Gent Rexha <gentr@stud.ntnu.no>
2016-10-08 04:15:29 +0200

    fixed lightning test

---
+++ app/src/main/java/rks/fiek/akeniligjerata/lightiningActivity.java
@@ -0,0 +1,78 @@
+package rks.fiek.akeniligjerata;
+
+import android.graphics.Typeface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class lightiningActivity extends AppCompatActivity {
+
+    TextView txvInfo;
+    TextView txvInfo2;
+    float LightValue = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lightining);
+
+        Typeface tfBlackboard = Typeface.createFromAsset(getAssets(),"fonts/BlackBoard.ttf");
+        txvInfo = (TextView)findViewById(R.id.txvInfo);
+        txvInfo.setTypeface(tfBlackboard);
+
+        txvInfo2 = (TextView)findViewById(R.id.txvInfo2);
+        txvInfo2.setTypeface(tfBlackboard);
+
+        SensorManager mySensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        Sensor LightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        if(LightSensor != null){
+            mySensorManager.registerListener(
+                    LightSensorListener,
+                    LightSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    public void btnTestLightOnClick(View v)
+    {
+        if(LightValue <= 8)
+        {
+            txvInfo2.setText(R.string.bad_lightning);
+        }
+        else if(LightValue > 30 && LightValue <= 120)
+        {
+            txvInfo2.setText(R.string.ok_lightning);
+        }
+        else if ( LightValue > 120 && LightValue <= 250)
+        {
+            txvInfo2.setText(R.string.good_lightning);
+        }
+        else
+        {
+            txvInfo2.setText(R.string.verygood_lightning);
+        }
+    }
+
+    private final SensorEventListener LightSensorListener = new SensorEventListener(){
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+                LightValue = event.values[0];
+            }
+        }
+
+    };
+}
--- app/src/main/java/rks/fiek/akeniligjerata/mainActivity.java
+++ app/src/main/java/rks/fiek/akeniligjerata/mainActivity.java
@@ -25,7 +25,7 @@ public class mainActivity extends AppCompatActivity
     private int locationRequestCode;
     TextView txvTitle;
     private DBHelper objDB;
-    float testLight = 15;
+    // float testLight = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -42,16 +42,6 @@ public class mainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SensorManager mySensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-
-        Sensor LightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
-        if(LightSensor != null){
-            mySensorManager.registerListener(
-                    LightSensorListener,
-                    LightSensor,
-                    SensorManager.SENSOR_DELAY_NORMAL);
-        }
 
 
         Typeface tfBlackboard = Typeface.createFromAsset(getAssets(),"fonts/BlackBoard.ttf");
@@ -80,43 +70,9 @@ public class mainActivity extends AppCompatActivity
 
     public void btnTestLight(View view)
     {
-        if(testLight <= 8)
-        {
-            Toast.makeText(getApplicationContext(), "The light in this place is not preferable to learn.", Toast.LENGTH_SHORT).show();
-        }
-        else if(testLight > 30 && testLight <= 120)
-        {
-            Toast.makeText(getApplicationContext(), "The light in this place is almost good to learn.", Toast.LENGTH_SHORT).show();
-        }
-        else if ( testLight > 120 && testLight <= 250)
-        {
-            Toast.makeText(getApplicationContext(), "The light in this place is good to learn.", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            Toast.makeText(getApplicationContext(), "The light in this place is very good to learn.", Toast.LENGTH_SHORT).show();
-        }
-
-
+        Intent intLightning = new Intent(this, lightiningActivity.class);
+        startActivity(intLightning);
     }
-
-    private final SensorEventListener LightSensorListener
-            = new SensorEventListener(){
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            if(event.sensor.getType() == Sensor.TYPE_LIGHT){
-                testLight = event.values[0];
-            }
-        }
-
-    };
 
     public void btnMapOnClick(View v)
     {
